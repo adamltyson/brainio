9df8cfde0e678589061d411627b8708e27db7b4e
Adam Tyson <adam.tyson@ucl.ac.uk>
2019-12-16 10:17:17 +0000

    bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 
 setup(
     name="brainio",
-    version="0.0.7",
+    version="0.0.8",
     description="Loading and saving of brain imaging data.",
     install_requires=requirements,
     extras_require={"dev": ["black", "pytest-cov", "pytest", "coveralls"]},
