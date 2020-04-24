056f03f55eb976eb177ce7ca87203407ed7131b1
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-04-24 11:57:03 +0100

    remove annoying warnings

--- brainio/__init__.py
+++ brainio/__init__.py
@@ -1,16 +1,4 @@
-__version__ = "0.0.11"
+__version__ = "0.0.13"
 __author__ = "Adam Tyson, Charly Rousseau"
 __license__ = "GPL-3.0"
 __name__ = "brainio"
-
-import luddite
-from packaging import version
-
-most_recent_version = luddite.get_version_pypi(__name__)
-
-if version.parse(__version__) < version.parse(most_recent_version):
-    print(
-        f"This version of {__name__} ({__version__}) is not the most recent "
-        f"version. Please update to v{most_recent_version} by running: "
-        f"'pip install {__name__} --upgrade'"
-    )
