8afbeb29fad562037d4bc9d668ef277499ad8cbb
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-05-27 12:44:33 +0100

    update travis

--- travis/install_unix.sh
+++
@@ -1,6 +0,0 @@
-wget $MINICONDA_URL -O miniconda.sh;
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda info -a
